df7beec81d855c5cba080956f0a00a47f7d2ceb1
Sapar Jumabekov <sapa4tech@gmail.com>
2021-12-18 15:41:55 +0300

    Initial Commit

---
+++ Dreamers_iOS/Dreamers_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dreamers_iOSApp.swift
+//  Dreamers_iOS
+//
+//  Created by user on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Dreamers_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
